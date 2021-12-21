f07869ccdebcfcd87df22be3a8a51019193478fb
esdras-schonevald <64711203+esdras-schonevald@users.noreply.github.com>
2021-12-21 00:09:59 -0300

    create comment for HttpConnector.php

--- src/bundles/adapters/HttpConnector.php
+++ src/bundles/adapters/HttpConnector.php
@@ -2,6 +2,11 @@
 
 namespace Adapters;
 
+/**
+ * HttpConnector is an adapter interface
+ * Adapters are usualy connectors to someone external service.
+ * To better understand why to use adapters look at this site https://refactoring.guru/design-patterns/adapter
+ */
 interface HttpConnector
 {
     public function getResponse(): \stdClass;
